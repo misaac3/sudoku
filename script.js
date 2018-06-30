1de7cbbb934759fe43d4d5cae5d5d990b636ec20
misaac3 <misaac3@wisc.edu>
2018-06-30 17:31:57 -0500

    SolveUsingBacktracking Succesful
    Sudoku puzzles can now be solved using a backtracking algorithm, but only with hardcoded boards.

--- script.js
+++ script.js
@@ -1,4 +1,210 @@
 
+var boardArray = new Array(9);
+for (var i = 0; i < boardArray.length; i++) {
+  boardArray[i] = new Array(9);
+}
+
+
+var board = new Array(9);
+for (var i = 0; i < board.length; i++) {
+  board[i] = new Array(9);
+}
+
+//console.log(boardArray);
+
+var isBoardGenerated = false;
+var generateBoardBtn = document.querySelector("#generateBoardBtn");
+
+// generateBoardBtn.addEventListener('click', generateBoardWithBacktracking1);
+generateBoardBtn.addEventListener('click', generateBoard);
+
+function listOfValidValues(col, row){
+  var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  for (var i = 0; i < board.length; i++) {
+    if(board[col][i] != undefined && array.includes(board[col][i])){
+      var index = array.indexOf(boardArray[col][i]);
+      array.splice(index, 1);
+    }
+  }
+
+    for (var i = 0; i < board[i].length; i++) {
+      if(board[i][row] != undefined && array.includes(board[i][row])){
+        var index = array.indexOf(board[i][row]);
+        array.splice(index, 1);
+      }
+    }
+
+    var newCol = 0;
+    if(col <= 2){
+      newCol = 0;
+    }
+    else if (col <= 5) {
+        newCol = 3;
+    }
+    else if (col <= 8) {
+        newCol = 6;
+    }
+
+    var newRow = 0;
+    if(row <= 2){
+      newRow = 0;
+    }
+    else if (row <= 5) {
+        newRow = 3;
+    }
+    else if (row <= 8) {
+        newRow = 6;
+    }
+
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        if(board[col + i][row + j] != undefined && array.includes(board[col + i][row + j])){
+          var index = array.indexOf(board[col + i][row + j]);
+          array.splice(index, 1);
+        }
+      }
+    }
+
+    return array;
+
+}
+function listOfValidValues(col, row, listOfInvalidValues){
+  var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  for (var i = 0; i < listOfInvalidValues.length; i++) {
+    var index = array.indexOf(listOfInvalidValues[i]);
+    array.splice(index, 1);
+  }
+
+  for (var i = 0; i < board.length; i++) {
+    if(board[col][i] != undefined && array.includes(board[col][i])){
+      var index = array.indexOf(boardArray[col][i]);
+      array.splice(index, 1);
+    }
+  }
+
+    for (var i = 0; i < board[i].length; i++) {
+      if(board[i][row] != undefined && array.includes(board[i][row])){
+        var index = array.indexOf(board[i][row]);
+        array.splice(index, 1);
+      }
+    }
+
+    var newCol = 0;
+    if(col <= 2){
+      newCol = 0;
+    }
+    else if (col <= 5) {
+        newCol = 3;
+    }
+    else if (col <= 8) {
+        newCol = 6;
+    }
+
+    var newRow = 0;
+    if(row <= 2){
+      newRow = 0;
+    }
+    else if (row <= 5) {
+        newRow = 3;
+    }
+    else if (row <= 8) {
+        newRow = 6;
+    }
+
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        if(board[col + i][row + j] != undefined && array.includes(board[col + i][row + j])){
+          var index = array.indexOf(board[col + i][row + j]);
+          array.splice(index, 1);
+        }
+      }
+    }
+
+    return array;
+
+}
+
+function isValidInsert(col, row, value) {
+//  console.log("(inside validInsert) col: " + col + " row: " + row + " value: " + value );
+  for (var i = 0; i < 9; i++) {
+    //console.log("boardArray[col][i] == value" + boardArray[col][i] == value + "||");
+    //console.log("boardArray[i][row] == value" + boardArray[i][row] == value);
+    if (board[col][i] == value || board[i][row] == value) {
+
+      return false;
+    }
+
+  }
+
+  var boxCol = Math.floor(col / 3) * 3;
+  var boxRow = Math.floor(row / 3) * 3;
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (board[boxCol + i][boxRow + j] == value) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function doFillInValue(col, row, value) {
+  board[col][row] = value;
+  dpUpdateHTML(col, row, value);
+}
+
+function doUpdateHTML(col, row, value) {
+//  console.log("(inside updateHTML) col: " + col + " row: " + row + " value: " + value );
+  var IDString = "#b" + col.toString() + row.toString();
+//  console.log("(inside updateHTML) IDString: "  + IDString);
+  var square = document.querySelector(IDString);
+//  console.log("tempCount: " + tempCount);
+//  console.log("value: " + value);
+  var valStr = value.toString();
+  square.innerHTML = valStr;
+//  console.log("square.innerHTML: " + square.innerHTML);
+
+}
+
+function generateBoard() {
+  for (var i = 0; i < boardArray.length; i++) {
+    for (var j = 0; j < boardArray[i].length; j++) {
+
+      var validNums = listOfValidValues(i, j);
+
+      if(validNums.length == 0){
+
+        // if(j==0){
+        //   j = boardArray[i].length - 1 - 1;
+        //   i--;
+        // }
+        // else {
+        //   j--;
+        // }
+        //
+        // continue;
+      }
+
+      var newIndex = Math.floor(Math.random() * validNums.length);
+      var newValue = validNums[newIndex];
+
+      if (isValidInsert(i, j, newValue)) {
+        doFillInValue(i, j, newValue)
+      }
+      else{
+
+      }
+
+
+    }
+  }
+
+}
+
+
+
 //this variable will update to the ID of the individual square that is clicked
 var currentID = "";
 
@@ -6,36 +212,287 @@ var currentID = "";
 var numberSquares = document.querySelectorAll(".number-square");
 
 //This will add an event listener to each number-square element
-for(var i = 0; i < numberSquares.length; i++){
-    var id = numberSquares[i].id;
-    numberSquares[i].addEventListener('click', promptNumberChooser);
+for (var i = 0; i < numberSquares.length; i++) {
+  var id = numberSquares[i].id;
+  numberSquares[i].addEventListener('click', promptNumberChooser);
 }
-
-
-
 
 //prompts the number chooser div when a number square is clicked
 function promptNumberChooser(event) {
-    var nct = document.querySelector("#numberChooserTable");
-    nct.style.display = "block";
-    var id = event.target.id;
-    currentID = id;
-    addEventListenersToNumberChooser();
+  var nct = document.querySelector("#numberChooserTable");
+  nct.style.display = "block";
+  var id = event.target.id;
+  currentID = id;
+  addEventListenersToNumberChooser();
 }
 
 //add event listeners to number prompt
 var numberChoosers = document.querySelectorAll(".number-chooser-data");
-function addEventListenersToNumberChooser(){;
+
+function addEventListenersToNumberChooser() {;
   for (var i = 0; i < numberChoosers.length; i++) {
     numberChoosers[i].addEventListener('click', changeContent);
   }
 }
 
 //changes the content of a number sqaure to the number clicked on in the nct
-function changeContent(e){
+function changeContent(e) {
+  var split = currentID.split("");
+  var col = split[1];
+  var row = split[2];
   var IDString = "#" + currentID;
   var square = document.querySelector(IDString);
   var newContent = event.target.innerHTML;
-  square.innerHTML = newContent;
+  if (validInsert(col, row, newContent)) {
+    square.innerHTML = newContent;
+  } else {
+    console.log("NO! BAD MOVE!");
+  }
+}
 
+// var board = new Array(9);
+// for (var i = 0; i < board.length; i++) {
+//   board[i] = new Array(9);
+// }
+
+var possValBoard = new Array(9);
+for (var i = 0; i < possValBoard.length; i++) {
+  possValBoard[i] = new Array(9);
+  for (var j = 0; j < possValBoard[i].length; j++) {
+    possValBoard[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  }
+
+}
+
+// console.log(possValBoard);
+// possValBoard.splice(0,1);
+// console.log(possValBoard);
+// possValBoard[0].splice(0,1);
+// console.log(possValBoard);
+// possValBoard[0][0].splice(0,1);
+//
+// console.log(possValBoard);
+// possValBoard = possValBoard[0][0].splice(0,1);
+// console.log(possValBoard);
+
+var insertsStack = new Array();
+
+var missedCoords = new Array(81);
+
+
+var count = 0;
+
+var tempCount = 0;
+
+/*
+each cell will have a possible value array/list (PVA)
+
+1.)start at first cell (0, 0)
+2.)choose a value from the cell's PVA
+3.)add it to the board and remove from PVA
+4.)update the rest of the tables PVAs
+5.)check if any remaining PVAs have length of 1 (ie that cell can only be 1 #)
+5a.)if yes, go to 3.) and repeat until no
+6.)repeat until the board is complete
+
+*/
+
+function generateBoardWithBacktracking1() {
+  for (var i = 0; i < boardArray.length; i++) {
+    for (var j = 0; j < boardArray[i].length; j++) {
+      console.log("i: " + i + " j: " + j);
+      console.log(possValBoard);
+
+      var currPossValArray = possValBoard[i][j];
+
+      if(currPossValArray.length == 0){
+          console.log("currPossValArray.length == 0");
+        //  if(i)
+
+
+      }
+
+      //2.)
+      var newIndex = Math.floor(Math.random() * currPossValArray.length);
+      var newValue = currPossValArray[newIndex];
+
+      //
+      // while(newValue == undefined ){
+      //   newIndex = Math.floor(Math.random() * currPossValArray.length);
+      //   newValue = currPossValArray[newIndex];
+      //   console.log("i am in the while loop");
+      // }
+
+      console.log("newIndex: " + newIndex);
+      console.log("newValue: " + newValue);
+      console.log("Step 2 executed");
+
+      //3.)
+      if (validInsert(i, j, newValue)) {
+        fillInValue(i, j, newValue)
+        console.log("Step 3 executed");
+      }
+      console.log("After validInsert() & fillInValue() -> Poss Val Array of [0][8]: " + possValBoard[0][8]);
+
+      //4.)
+      updatePossValArray(i, j, newValue);
+      console.log("Step 4 executed");
+      console.log("After updatePossValArray() -> Poss Val Array of [0][8]: " + possValBoard[0][8]);
+
+      //5.)
+    checkForSingularities();
+      console.log("Step 5 executed");
+
+      console.log("After checkForSingularities() -> Poss Val Array of [0][8]: " + possValBoard[0][8]);
+
+
+      console.log("----------------------------");
+
+
+
+
+    }
+  }
+
+
+}
+
+// function generateBoardWithBacktracking() {
+//   for (var i = 0; i < boardArray.length; i++) {
+//     for (var j = 0; j < boardArray[i].length; j++) {
+//       if (!(boardArray[i][j] == undefined)) {
+//         count++;
+//
+//         console.log("i: " + i + " j: " + j);
+//         console.log(possValBoard);
+//
+//         var currPossValArray = possValBoard[i][j];
+//         //  var randValue = Math.floor(Math.random() * 9) + 1;
+//         var valIndex = Math.floor(Math.random() * currPossValArray.length);
+//         var randValue = currPossValArray[valIndex];
+//
+//
+//         if (validInsert(i, j, randValue)) {
+//           fillInValue(i, j, randValue);
+//           currPossValArray.splice(valIndex, 1);
+//           updatePossValArray(i, j, randValue);
+//           checkForSingularities();
+//
+//         } else {
+//           missedCoords.push([i, j]);
+//
+//         }
+//
+//
+//
+//         ///  if(count > 500) break;
+//
+//       }
+//     }
+//   }
+// }
+
+
+function validInsert(col, row, value) {
+  console.log("(inside validInsert) col: " + col + " row: " + row + " value: " + value );
+  for (var i = 0; i < 9; i++) {
+    //console.log("boardArray[col][i] == value" + boardArray[col][i] == value + "||");
+    //console.log("boardArray[i][row] == value" + boardArray[i][row] == value);
+    if (boardArray[col][i] == value || boardArray[i][row] == value) {
+
+      return false;
+    }
+
+  }
+
+  var boxCol = Math.floor(col / 3) * 3;
+  var boxRow = Math.floor(row / 3) * 3;
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (boardArray[boxCol + i][boxRow + j] == value) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function fillInValue(col, row, value) {
+  tempCount++;
+  console.log("(inside fillInValue) col: " + col + " row: " + row + " value: " + value );
+
+  var obj = {col: col, row: row, value: value, boardArray: boardArray, possValBoard: possValBoard};
+  var deepCopy = JSON.parse(JSON.stringify(obj));
+
+
+  insertsStack.push(deepCopy);
+
+  console.log("insertsStack: " + JSON.stringify(insertsStack[insertsStack.length - 1]));
+  boardArray[col][row] = value;
+  updateHTML(col, row, value);
+}
+
+function updateHTML(col, row, value) {
+  console.log("(inside updateHTML) col: " + col + " row: " + row + " value: " + value );
+  var IDString = "#b" + col.toString() + row.toString();
+  console.log("(inside updateHTML) IDString: "  + IDString);
+  var square = document.querySelector(IDString);
+  console.log("tempCount: " + tempCount);
+  console.log("value: " + value);
+  var valStr = value.toString();
+  square.innerHTML = valStr;
+  console.log("square.innerHTML: " + square.innerHTML);
+
+}
+
+function checkForSingularities() {
+  var singularityExists = false;
+  do {
+  singularityExists = false;
+    for (var i = 0; i <  possValBoard.length; i++) {
+      for (var j = 0; j < possValBoard[i].length; j++) {
+//console.log("Inside checkForSingularities: ");
+  //      console.log("i: " + i + " j: " + j);
+if(i === 8 && j === 0){
+        console.log(possValBoard[i][j]);
+      }
+        if (possValBoard[i][j].length === 1 && boardArray[i][j] == undefined) {
+          console.log("here");
+          fillInValue(i, j, possValBoard[i][j][0]);
+          updatePossValArray(i, j, possValBoard[i][j][0]);
+          possValBoard[i][j].splice(0, 1);
+        //  singularityExists = true;
+        }
+      }
+    }
+  } while (singularityExists);
+  return singularityExists;
+}
+
+function updatePossValArray(col, row, val) {
+  for (var i = 0; i < 9; i++) {
+    if (possValBoard[col][i].includes(val)) {
+      var ind = possValBoard[col][i].indexOf(val);
+      console.log("(in updatePossValArray())Removing this value: " + possValBoard[col][i][ind]);
+      possValBoard[col][i].splice(ind, 1);
+    }
+    if (possValBoard[i][row].includes(val)) {
+      var ind = possValBoard[i][row].indexOf(val);
+      possValBoard[i][row].splice(ind, 1);
+    }
+  }
+
+  var boxCol = Math.floor(col / 3) * 3;
+  var boxRow = Math.floor(row / 3) * 3;
+
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (possValBoard[boxCol + i][boxRow + j].includes(val)) {
+        var ind = possValBoard[boxCol + i][boxRow + j].indexOf(val);
+        possValBoard[boxCol + i][boxRow + j].splice(ind, 1);
+      }
+    }
+  }
+
+  return possValBoard;
 }
---
+++ solve.js
@@ -0,0 +1,240 @@
+/*
+Board indices visulized
+|--------------------------|
+| 0| 1| 2| 3| 4| 5| 6| 7| 8|
+|--------------------------|
+| 9|10|11|12|13|14|15|16|17|
+|--------------------------|
+|18|19|20|21|22|23|24|25|26|
+|--------------------------|
+|27|28|29|30|31|32|33|34|35|
+|--------------------------|
+|36|37|38|39|40|41|42|43|44|
+|--------------------------|
+|45|46|47|48|49|50|51|52|53|
+|--------------------------|
+|54|55|56|57|58|59|60|61|62|
+|--------------------------|
+|63|64|65|66|67|68|69|70|71|
+|--------------------------|
+|72|73|74|75|76|77|78|79|80|
+|--------------------------|
+
+var str = "|--------------------------|\n|";
+for (var i = 0; i < 9; i++) {
+for (var j = 0; j < 9; j++) {
+if((i *9) + j < 10){
+str += " " + ((i *9) + j).toString() + "|";
+}
+else {
+str += ((i *9) + j).toString() + "|";
+}
+}
+str += "\n|--------------------------|\n|";
+}
+console.log(str);
+*/
+
+//var board = new Array(81);
+var buttonSolve = document.querySelector("#consoleSolve");
+var buttonPrint = document.querySelector("#printBoard2D");
+buttonSolve.addEventListener("click", solveWithBacktrackingDriver);
+buttonPrint.addEventListener("click", printBoard2D);
+
+function solveWithBacktrackingDriver(){
+  var h1 = document.querySelector("#message");
+  h1.innerHTML = "Solving...";
+  if(solveWithBacktracking(board2D)){
+    console.log("successful");
+    console.log("After completion: ");
+    printBoard2D();
+    updateHTML();
+    h1.innerHTML = "Success"
+  }
+  else{
+    console.log("FAIL");
+    h1.innerHTML = "No Solution found";
+  }
+}
+//First test
+// board =
+// [8, 0, 0, 0, 0, 0, 3, 0, 0,
+//   4, 0, 0, 8, 0, 0, 2, 6, 7,
+//   0, 6, 0, 0, 5, 2, 0, 0, 1,
+//   0, 0, 0, 0, 1, 9, 0, 0, 8,
+//   0, 3, 1, 4, 0, 8, 5, 2, 0,
+//   9, 0, 0, 2, 3, 0, 0, 0, 0,
+//   5, 0, 0, 9, 7, 0, 0, 1, 0,
+//   6, 7, 4, 0, 0, 1, 0, 0, 2,
+//   0, 0, 3, 0, 0, 0, 0, 0, 4];
+
+// updateHTML();
+var BOARD2D;
+var board2D =[
+  [8, 0, 0, 0, 0, 0, 3, 0, 0],
+  [4, 0, 0, 8, 0, 0, 2, 6, 7],
+  [0, 6, 0, 0, 5, 2, 0, 0, 1],
+  [0, 0, 0, 0, 1, 9, 0, 0, 8],
+  [0, 3, 1, 4, 0, 8, 5, 2, 0],
+  [9, 0, 0, 2, 3, 0, 0, 0, 0],
+  [5, 0, 0, 9, 7, 0, 0, 1, 0],
+  [6, 7, 4, 0, 0, 1, 0, 0, 2],
+  [0, 0, 3, 0, 0, 0, 0, 0, 4]
+];
+
+// board2D =[
+//   [1, 0, 0, 0, 0, 0, 3, 0, 0],
+//   [0, 0, 0, 8, 0, 0, 2, 6, 7],
+//   [0, 6, 0, 0, 5, 2, 0, 0, 1],
+//   [0, 0, 0, 0, 1, 9, 0, 0, 8],
+//   [0, 3, 1, 4, 0, 8, 5, 2, 0],
+//   [9, 0, 0, 2, 3, 0, 0, 0, 0],
+//   [5, 0, 0, 9, 7, 0, 0, 1, 0],
+//   [6, 7, 4, 0, 0, 1, 0, 0, 2],
+//   [0, 0, 3, 0, 0, 0, 0, 0, 4]
+// ];
+
+//printBoard2D();
+function printBoard2D(){
+  var str = "___________________\n";
+  for (var i = 0; i < board2D.length; i++) {
+    str += "|";
+    for (var j = 0; j < board2D[i].length; j++) {
+      if(board2D[i][j] == 0){
+        str += " |"
+      }
+      else{
+        str += board2D[i][j].toString() + "|";
+      }
+    }
+
+    if(i % 3 == 2){
+      str += "\n|_____|_____|_____|\n"
+    }
+    // if (i == board2D.length - 1) {
+    //   str += "___________________";
+    // }
+    else{
+      str += "\n|-----|-----|-----|\n"
+    }
+  }
+  console.log(str);
+}
+
+
+function printBoard(){
+  var stringBuilder = "|-----------------|\n";
+  var charCount = 0;
+  for (var i = 0; i < board.length; i++) {
+    stringBuilder += "|" + board[i].toString();
+    charCount++;
+    if(charCount >= 9){
+      stringBuilder += "|\n|-----------------|\n";
+      charCount = 0;
+    }
+  }
+  console.log(stringBuilder);
+}
+
+//TODO
+/*
+
+backtracking explaination from Jeff Erickson's Textbook
+(http://jeffe.cs.illinois.edu/teaching/algorithms/notes/03-backtracking.pdf)
+Find a small choice whose correct answer would reduce the problem
+size. For each possible answer, temporarily adopt that choice and
+recurse. (Don’t try to be clever about which choices to try; just try
+them all.) The recursive subproblem is often more general than the
+original target problem;  in each recursive subproblem, we must
+consider only solutions that are consistent with the choices we have
+already made
+*/
+function solveWithBacktracking(board2D){
+  BOARD2D = board2D;
+  printBoard2D();
+  updateHTML();
+  //find open square (if none, solution is found!)
+  //if one is found assign the row and column to variables
+  var obj = {row: undefined, col: undefined};
+
+  if(!findNextOpenSquare(board2D, obj)){
+    return true;
+  }
+
+  //try 1 through 9 in the open square
+  for (var tryValue = 1; tryValue <= 9; tryValue++) {
+
+    //if it is a valid move, insert the value into the board
+    //and rescursively call this function with the new board
+    if(isValidInsert(obj.row, obj.col, tryValue)){
+
+      board2D[obj.row][obj.col] = tryValue;
+    //  doUpdateHTML(obj.row, obj.col, tryValue);
+
+      //if that call is successful return true
+      if(solveWithBacktracking(board2D)){
+        return true;
+      }
+      //if not undo the insertion made earlier
+      else{
+        board2D[obj.row][obj.col] = 0;
+      }
+    }
+  }
+
+  //return false at the end of the function to trigger backtracking
+
+  return false
+}
+
+function findNextOpenSquare(_board, coords){//, row, col){
+
+  for (var i = 0; i < _board.length; i++) {
+    for (var j = 0; j < _board[i].length; j++) {
+      if(_board[i][j] == 0){
+        coords.row = i;
+        coords.col = j;
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+
+
+function isValidInsert(col, row, value) {
+  for (var i = 0; i < 9; i++) {
+    if (board2D[col][i] == value || board2D[i][row] == value) {
+      return false;
+    }
+  }
+
+  var boxCol = Math.floor(col / 3) * 3;
+  var boxRow = Math.floor(row / 3) * 3;
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (board2D[boxCol + i][boxRow + j] == value) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
+
+function updateHTML(){
+  for(var row = 0; row < 9; row++){
+    for(var col = 0; col < 9; col++){
+      var value = board2D[row][col];
+      if(value == 0) value = " "
+      var IDString = "#b" + col.toString() + row.toString();
+      var square = document.querySelector(IDString);
+      var valStr = value.toString();
+      square.innerHTML = valStr;
+    }
+  }
+
+}
+updateHTML();
